2410ae0985001b268ea047c7fa513099364bd1f4
Billy1339 <william.m.sargent@gmail.com>
2015-02-17 16:01:28 -0500

    added author input field, and working on submitting new post

--- app/js/controllers/blog.controller.js
+++ app/js/controllers/blog.controller.js
@@ -1,13 +1,27 @@
 angular.module('Teewinot').controller('BlgoCtrl', function($scope, $http, PostFactory) {
   'use strict'
 
-  var promise;
+  var promise, clearForm;
 
   promise = PostFactory.fetch();
   promise.then(function(posts) {
     $scope.posts = posts
-    debugger
+
   })
+
+  clearForm = function() {
+    $scope.post = {};
+  };
+
+  $scope.createPost = function(data) {
+    var params;
+    params = data;
+    $http.post('localhost:3000/posts').success(function(response) {
+      console.log(response);
+      clearForm();
+    })
+
+  }
 
 
 });
